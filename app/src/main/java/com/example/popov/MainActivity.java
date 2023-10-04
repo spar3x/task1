54d21420d75c6fef523b9be9d6e062d88dd59b28
rmp <ya>
2023-10-04 18:14:33 +0300

    fwefnew

---
+++ app/src/main/java/com/example/popov/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.popov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(this, OnBoard1.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/popov/OnBoard1.java
@@ -0,0 +1,20 @@
+package com.example.popov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class OnBoard1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_board1);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(this, OnBoard2.class);
+        startActivity(intent);
+    }
+}
